b5a8d6b6fac5413a4b29db7c528b3022f109a795
rony646 <rony646@users.noreply.github.com>
2021-06-06 19:16:25 -0300

    adding tech icons

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -3,22 +3,31 @@ import React from 'react';
 
 import { Container } from './styles';
 
+import techBgImg from '../../assets/tech-bg.png';
+
 const HomePage: React.FC = () => {
     return(
         <Container>
-            <h1>
-                HI, I’M RONY A <br/>FRONTEND DEVELOPER <br/>FROM BRAZIL!
-            </h1>
+            <div className="content-container">
+                <h1>
+                    HI, I’M RONY A <br/>FRONTEND DEVELOPER <br/>FROM BRAZIL!
+                </h1>
 
-            <h2>{`<About me />`}</h2>
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                Aliquam turpis in egestas consequat molestie. Integer molestie pellentesque 
-                consequat vitae pulvinar orci sit. Placerat donec pulvinar id vitae. 
-                Id rhoncus, senectus aliquet pellentesque elementum.
-            </p>
+                <h2>{`<About me />`}</h2>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                    Aliquam turpis in egestas consequat molestie. Integer molestie pellentesque 
+                    consequat vitae pulvinar orci sit. Placerat donec pulvinar id vitae. 
+                    Id rhoncus, senectus aliquet pellentesque elementum.
+                </p>
 
-            <a href="/">{`<Talk to me />`}</a>
+                <a href="/">{`<Talk to me />`}</a>
+
+            </div>
+
+            <div className="img-container">
+               <img src={techBgImg} alt="tech logos" />
+            </div>
         </Container>
     );
 };
--- src/pages/home/styles.tsx
+++ src/pages/home/styles.tsx
@@ -1,52 +1,100 @@
 import stlyed from 'styled-components';
 
 export const Container = stlyed.div`
-    position: relative;
     display: flex;
-    flex-direction: column;
-    justify-content: center;
-    width: 100%;
-    height: 100%;
-    box-sizing: border-box;
-    padding-left: 40px;
-    top: 80px;
     
-    @media(max-width: 768px) {
-        top: 10px;
-        // background: green;
+    .img-container {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 50%;
+        // background: #53a85856;
+
+        @media(max-width: 1024px) {
+            display: none;
+            width: 0%;
+        }
+    }
+
+    .content-container {
+        // background: #ff000057;
+        position: relative;
+        display: flex;
+        flex-direction: column;
+        // background: #8080804c;
+        justify-content: center;
+        aling-items: center;
+        width: 50%;
+        height: 100vh;
+        box-sizing: border-box;
+        padding-left: 120px;
+        
+        @media(max-width: 768px) {
+            justify-content: flex-start;
+            padding-left: 40px;
+        }
+
+        @media(max-width: 1024px) {
+           width: 100%;
+        }
+       
+        
+        h1 {
+            color: #15b325;
+            font-size: 2.5em;
+    
+             
+            @media(max-width: 768px) {
+               font-size: 2.3em;
+            }
+        }
+    
+        h1, a {
+            transition: color 0.3s;
+        }
+    
+        h2, p {
+            color: #fff;
+            transition: color 0.3s;
+    
+            &:hover {
+                color: #c4c3c3;
+            }
+        }
+        
+        h2 {
+            margin: 0;
+            margin-top: 20px;
+        }
+    
+        a, h1 {
+            color: #15b325;
+            font-weight: bold;
+            text-decoration: none;
+            cursor: pointer;
+           
+            &:hover {
+                color: #05811096;
+            }
+        }
+    
+    
+      
+    
+        p {
+            width: 60%;
+            text-align: left;
+    
+            @media(max-width: 768px) {
+                width: 70%;
+            }
+        }
+    
+        a {
+            margin-top: 20px;
+            font-size: 1.7em
+        }
+
     }
    
-    
-    h1 {
-        color: #149021;
-        font-size: 2.3em;
-    }
-
-    a, h1 {
-        color: #149021;
-        font-weight: bold;
-        text-decoration: none;
-        cursor: pointer;
-       
-        &:hover {
-            color: #046b0c97;
-        }
-    }
-
-    h2, p {
-        color: #fff;
-    }
-
-    p {
-        width: 40%;
-        text-align: left;
-
-        @media(max-width: 768px) {
-            width: 70%;
-        }
-    }
-
-    a {
-        font-size: 1.4em
-    }
 `
